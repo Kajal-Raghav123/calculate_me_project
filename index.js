fe053065df291f7018fe4f053f60d18d8fb7cbfc
Kajal-Raghav123 <kajalraghav9368@gmail.com>
2025-05-24 19:13:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("number");
+const buttons = document.querySelectorAll(".container button");
+
+let expression = "";
+ buttons.forEach(button=>{
+    button.addEventListener("click",()=>{
+        const value = button.textContent;
+        switch(value){
+            
+        }
+    })
+ })
+
