a7d0ac5d4ad16d1fe64cf580ed69da035a4522c8
Kajal-Raghav123 <kajalraghav9368@gmail.com>
2025-06-06 22:28:18 +0530

    Update To-do project: modify index, script, styles and add new practice files

--- To-do_DOM_project/script.js
+++ To-do_DOM_project/script.js
@@ -7,11 +7,47 @@ buttons.addEventListener("click",()=>{
         list.appendChild(li);
         li.textContent = input.value;
         input.value = "";
+
+
+        //remove button
         const removeBtn = document.createElement("remove");
+
+        // Edit button
+        const edit = document.createElement("Edit");
+        li.appendChild(edit);
+        edit.textContent = "Edit";
+
+        edit.addEventListener("click",()=>{
+        const editInput = document.createElement("input");
+      
+        //Save button
+        const save = document.createElement("button");
+        save.textContent = "Save";
+        save.className = "remove";
+        save.addEventListener("click",()=>{
+        
+        })
+        console.log(li.children);
+        const editInputValue = li.firstChild.textContent;
+        console.log(editInputValue);
+          edit.replaceWith(save);
+        console.log(li.firstChild);
+       
+        li.replaceChild(editInput,li.firstChild);
+        console.log(input.value);
+        // editInput.setAttribute("value",editInput);
+        editInput.value = editInputValue;
+
+
+        
+        console.log(editInput.value);
+        li.appendChild(editInput);
+        })
         li.appendChild(removeBtn);
         removeBtn.textContent = "Delete";
         removeBtn.addEventListener("click",()=>{
             li.remove();
+           removeBtn.remove();
         });
         
      li.classList.toggle("highlight");
